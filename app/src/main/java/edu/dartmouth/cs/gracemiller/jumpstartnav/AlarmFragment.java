cf837f86eff9007e5707a6e2811f852ce5c99fce
gbzales <gbzales@gmail.com>
2016-03-05 00:43:32 -0500

    ksmlfkd

--- app/src/main/java/edu/dartmouth/cs/gracemiller/jumpstartnav/AlarmFragment.java
+++ app/src/main/java/edu/dartmouth/cs/gracemiller/jumpstartnav/AlarmFragment.java
@@ -12,8 +12,12 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.CardView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
@@ -64,6 +68,12 @@ public class AlarmFragment extends Fragment  {
     ArrayList<Recording> myRecordings;
     public static android.app.LoaderManager loaderManager;
 
+    public  AlarmAdapter mAlarmAdapter;
+    public  RecyclerView mRecyclerView;
+    public  RecyclerView.LayoutManager mLayoutManager;
+    public ArrayList<Alarm> mDataset;
+    public AlarmEntryDbHelper mAlarmDbHelper;
+
     private static final int ALARM_LOADER_ID = 1;
     private static final int RECORDING_LOADER_ID = 2;
 
@@ -90,16 +100,38 @@ public class AlarmFragment extends Fragment  {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        Alarm myAlarm = new Alarm();
+        myAlarm.setmAlarmType(0);
+        Calendar cal = Calendar.getInstance();
+        myAlarm.setmDateTime(cal);
+
+
+
+
         // Inflate the layout for this fragment
         mInflatedView = inflater.inflate(R.layout.fragment_alarm, container, false);
 
+
+        loaderManager = getActivity().getLoaderManager();
+        loaderManager.initLoader(1, null, alarmLoaderListener).forceLoad();
+
+        mRecyclerView = (RecyclerView) mInflatedView.findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(mContext);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+
+        Log.d("getting above fab", "above fab");
         FloatingActionButton fab = (FloatingActionButton) mInflatedView.findViewById(R.id.add_alarm);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Log.d("TAGG", "FAB click");
                 addNewAlarm();
             }
         });
+
 
         return mInflatedView;
     }
@@ -107,100 +139,102 @@ public class AlarmFragment extends Fragment  {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        LinearLayout cardList = (LinearLayout) view.findViewById(R.id.cardContainer);
 
-        final CardView alarmView = (CardView) view.findViewById(R.id.cardview);
-        final LinearLayout settingsView = (LinearLayout) view.findViewById(R.id.expandedView);
-        alarmView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!mOpened) {
-                    alarmView.setBackgroundColor(getResources().getColor(R.color.colorAccent));
-                    settingsView.setVisibility(View.VISIBLE);
-                    slide_down(mContext, settingsView);
-                    mOpened = true;
-                } else if (mOpened) {
-                    alarmView.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
-                    slide_up(mContext, settingsView);
-                    settingsView.setVisibility(View.GONE);
-                    mOpened = false;
-                    Toast.makeText(mContext, "Changes Not Saved", Toast.LENGTH_LONG).show();
-                }
-            }
-        });
-
-        final Switch alarmSwitch = (Switch) view.findViewById(R.id.alarm_on_switch);
-        alarmSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                isActivated = isChecked;
-            }
-        });
-
-        final ListView settingsList = (ListView) view.findViewById(R.id.alarmOptions);
-        settingsList.setAdapter(new ArrayAdapter<String>(mContext, R.layout.alarm_settings_list_text,
-                R.id.settings_list_white_text, alarmSettingsArray));
-        settingsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                switch (position) {
-                    case 0:
-                        onDateClicked();
-                        View dateView = settingsList.getChildAt(position);
-                        TextView dateTextView = (TextView) dateView.findViewById(R.id.settings_list_white_text);
-                        String date = android.text.format.DateFormat.format("MMM dd yyyy", mDateAndTime).toString();
-                        dateTextView.setText("Date: " + date);
-                        break;
-                    case 1:
-                        onActivityClicked();
-                        View activityView = settingsList.getChildAt(position);
-                        TextView activityTextView = (TextView) activityView.findViewById(R.id.settings_list_white_text);
-                        activityTextView.setText("Wakeup Activity: " + mActivityType);
-                        break;
-                    case 2:
-                        onRingtoneClicked();
-                        View ringtoneView = settingsList.getChildAt(position);
-                        TextView ringtoneTestView = (TextView) ringtoneView.findViewById(R.id.settings_list_white_text);
-                        ringtoneTestView.setText("Ringtone: " + mRingtone);
-                        break;
-                    case 3:
-                        onReminderClicked();
-                        break;
-                }
-
-            }
-        });
-
-        Button saveButton = (Button) view.findViewById(R.id.cardSaveButton);
-        saveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                alarmView.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
-                if (mOpened) {
-                    slide_up(mContext, settingsView);
-
-                    //ADD ALARM HERE
-                    //TODO: add alarm here
-
-                }
-                mOpened = false;
-                if (!mOpened) {
-                    settingsView.setVisibility(View.GONE);
-                    Toast.makeText(mContext, "Saved!", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-
-        Button deleteButton = (Button) view.findViewById(R.id.cardDeleteButton);
-        deleteButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
+//        LinearLayout cardList = (LinearLayout) view.findViewById(R.id.cardContainer);
+//
+//        final CardView alarmView = (CardView) view.findViewById(R.id.cardview);
+//        final LinearLayout settingsView = (LinearLayout) view.findViewById(R.id.expandedView);
+//        alarmView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (!mOpened) {
+//                    alarmView.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+//                    settingsView.setVisibility(View.VISIBLE);
+//                    slide_down(mContext, settingsView);
+//                    mOpened = true;
+//                } else if (mOpened) {
+//                    alarmView.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+//                    slide_up(mContext, settingsView);
+//                    settingsView.setVisibility(View.GONE);
+//                    mOpened = false;
+//                    Toast.makeText(mContext, "Changes Not Saved", Toast.LENGTH_LONG).show();
+//                }
+//            }
+//        });
+//
+//        final Switch alarmSwitch = (Switch) view.findViewById(R.id.alarm_on_switch);
+//        alarmSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                isActivated = isChecked;
+//            }
+//        });
+//
+//        final ListView settingsList = (ListView) view.findViewById(R.id.alarmOptions);
+//        settingsList.setAdapter(new ArrayAdapter<String>(mContext, R.layout.alarm_settings_list_text,
+//                R.id.settings_list_white_text, alarmSettingsArray));
+//        settingsList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                switch (position) {
+//                    case 0:
+//                        onDateClicked();
+//                        View dateView = settingsList.getChildAt(position);
+//                        TextView dateTextView = (TextView) dateView.findViewById(R.id.settings_list_white_text);
+//                        String date = android.text.format.DateFormat.format("MMM dd yyyy", mDateAndTime).toString();
+//                        dateTextView.setText("Date: " + date);
+//                        break;
+//                    case 1:
+//                        onActivityClicked();
+//                        View activityView = settingsList.getChildAt(position);
+//                        TextView activityTextView = (TextView) activityView.findViewById(R.id.settings_list_white_text);
+//                        activityTextView.setText("Wakeup Activity: " + mActivityType);
+//                        break;
+//                    case 2:
+//                        onRingtoneClicked();
+//                        View ringtoneView = settingsList.getChildAt(position);
+//                        TextView ringtoneTestView = (TextView) ringtoneView.findViewById(R.id.settings_list_white_text);
+//                        ringtoneTestView.setText("Ringtone: " + mRingtone);
+//                        break;
+//                    case 3:
+//                        onReminderClicked();
+//                        break;
+//                }
+//
+//            }
+//        });
+//
+//        Button saveButton = (Button) view.findViewById(R.id.cardSaveButton);
+//        saveButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                alarmView.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+//                if (mOpened) {
+//                    slide_up(mContext, settingsView);
+//
+//                    //ADD ALARM HERE
+//                    //TODO: add alarm here
+//
+//                }
+//                mOpened = false;
+//                if (!mOpened) {
+//                    settingsView.setVisibility(View.GONE);
+//                    Toast.makeText(mContext, "Saved!", Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        });
+//
+//        Button deleteButton = (Button) view.findViewById(R.id.cardDeleteButton);
+//        deleteButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
     }
 
     private void addNewAlarm() {
+        Log.d("addNewAlarm()", "add");
         // Create dialog to show current time
         TimePickerDialog.OnTimeSetListener mTimeListener = new TimePickerDialog.OnTimeSetListener() {
             public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
@@ -395,63 +429,66 @@ public class AlarmFragment extends Fragment  {
 
     };
 
-    private LoaderManager.LoaderCallbacks<ArrayList<Recording>> alarmLoaderListener
-            = new LoaderManager.LoaderCallbacks<ArrayList<Recording>>() {
+    private LoaderManager.LoaderCallbacks<ArrayList<Alarm>> alarmLoaderListener
+            = new LoaderManager.LoaderCallbacks<ArrayList<Alarm>>() {
         @Override
         public Loader onCreateLoader(int id, Bundle args) {
-            Log.d("onCreateRecLoader()", "onCreateRecordingLoader()");
+            Log.d("onALARMLOADERCREATE()", "onALARM LOADER CREATE()");
 
             // returns an entry loader using context
             return new AlarmLoader(mContext);
         }
 
         @Override
-        public void onLoadFinished(Loader<ArrayList<Recording>> loader, ArrayList<Recording> data) {
+        public void onLoadFinished(Loader<ArrayList<Alarm>> loader, ArrayList<Alarm> data) {
+            Log.d("onLoaderFinished()", "onLoaderFinishALARM()");
+
             //sets global variable
-            myRecordings = data;
+            mDataset = data;
 
-            if(!data.isEmpty()) {
-                Log.d("onLoadFinished()", "not empty");
+//            if(!data.isEmpty()) {
+//                Log.d("onLoadFinished()", "not empty");
+//
+//                //String[] recordingNames = new String[40];
+//                ArrayList<String> recordingNames = new ArrayList<String>();
+//                int i = 0;
+//                for (Recording recording : data) {
+//                    Log.d("in recordings", "recording: " + recording.getAlarmName());
+//                    //recordingNames.add(recording.getAlarmName());
+//                    //recordingNames[i] = recording.getAlarmName();
+//                    //i++;
+//                    recordingNames.add(recording.getAlarmName());
+//                    //Log.d("in recordings", "recording: " + recordingNames[i]);
+//                    Log.d("in recordings", "recording: " + recordingNames.toArray());
+//
+//
+//                }
 
-                //String[] recordingNames = new String[40];
-                ArrayList<String> recordingNames = new ArrayList<String>();
-                int i = 0;
-                for (Recording recording : data) {
-                    Log.d("in recordings", "recording: " + recording.getAlarmName());
-                    //recordingNames.add(recording.getAlarmName());
-                    //recordingNames[i] = recording.getAlarmName();
-                    //i++;
-                    recordingNames.add(recording.getAlarmName());
-                    //Log.d("in recordings", "recording: " + recordingNames[i]);
-                    Log.d("in recordings", "recording: " + recordingNames.toArray());
+            //sets adapter to array list of exercises
+
+            // Define a new adapter
+//                myAdapter = new ArrayAdapter<String>(mContext,
+//                        R.layout.listview_layout, recordingNames);
+
+            mAlarmAdapter = new AlarmAdapter(mDataset);
+            Log.d("onalarmLoadFinished()", "got adapter");
 
 
-                }
-
-                //sets adapter to array list of exercises
-
-                // Define a new adapter
-                myAdapter = new ArrayAdapter<String>(mContext,
-                        R.layout.listview_layout, recordingNames);
-                Log.d("onLoadFinished()", "got adapter");
-
-
-                // Assign the adapter to ListView
-                //setListAdapter(mAdapter);
-                //myAdapter = new ExerciseLineArrayAdapter(mContext, data);
-                //mListView.setListAdapter(myAdapter);
-                mListView.setAdapter(myAdapter);
-                Log.d("onLoadFinished()", "set adapter");
-            }
+            // Assign the adapter to ListView
+            //setListAdapter(mAdapter);
+            //myAdapter = new ExerciseLineArrayAdapter(mContext, data);
+            //mListView.setListAdapter(myAdapter);
+            mRecyclerView.setAdapter(mAlarmAdapter);
+            Log.d("onalarmLoadFinished()", "set adapter");
         }
 
         @Override
-        public void onLoaderReset(Loader<ArrayList<Recording>> loader) {
-            Log.d("onLoaderReset()", "onLoaderReset()");
+        public void onLoaderReset(Loader<ArrayList<Alarm>> loader) {
+            Log.d("onalarmLoaderReset()", "onLoaderReset()");
 
             //reloads exercises into adapter
-            myAdapter.clear();
-            myAdapter.notifyDataSetChanged();
+           // mAlarmAdapter.clear();
+            mAlarmAdapter.notifyDataSetChanged();
 
 
         }
@@ -522,6 +559,24 @@ public class AlarmFragment extends Fragment  {
             //reloads the list when onResume is called
             //loaderManager.initLoader(1, null, this).forceLoad();
         }
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Log.d("adding alarm", "add");
+            addNewAlarm();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
 
 
