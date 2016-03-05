0a06eccc682f1c2467550b4421d0a83f6ec30247
gracemiller <grace.a.miller.17@dartmouth.edu>
2016-03-04 22:24:13 -0500

    adapters

--- app/src/main/java/edu/dartmouth/cs/gracemiller/jumpstartnav/AlarmListAdapter.java
+++ app/src/main/java/edu/dartmouth/cs/gracemiller/jumpstartnav/AlarmListAdapter.java
@@ -1,55 +1,55 @@
-package edu.dartmouth.cs.gracemiller.jumpstartnav;
-
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
-public class AlarmAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
-    private String[] mDataset;
-
-    // Provide a reference to the views for each data item
-    // Complex data items may need more than one view per item, and
-    // you provide access to all the views for a data item in a view holder
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        // each data item is just a string in this case
-        public TextView mTextView;
-        public ViewHolder(TextView v) {
-            super(v);
-            mTextView = v;
-        }
-    }
-
-    // Provide a suitable constructor (depends on the kind of dataset)
-    public AlarmAdapter(String[] myDataset) {
-        mDataset = myDataset;
-    }
-
-    // Create new views (invoked by the layout manager)
-    @Override
-    public AlarmAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
-                                                   int viewType) {
-
-        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
-        View view = layoutInflater.inflate(R.layout.cardview_layout, parent, false);
-        ViewHolder viewHolder = new ViewHolder(view);
-
-        return viewHolder;
-    }
-
-    // Replace the contents of a view (invoked by the layout manager)
-    @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        // - get element from your dataset at this position
-        // - replace the contents of the view with that element
-        holder.mTextView.setText(mDataset[position]);
-
-    }
-
-    // Return the size of your dataset (invoked by the layout manager)
-    @Override
-    public int getItemCount() {
-        return mDataset.length;
-    }
-}}
+//package edu.dartmouth.cs.gracemiller.jumpstartnav;
+//
+//import android.support.v7.widget.RecyclerView;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.ImageView;
+//import android.widget.TextView;
+//public class AlarmAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+//    private String[] mDataset;
+//
+//    // Provide a reference to the views for each data item
+//    // Complex data items may need more than one view per item, and
+//    // you provide access to all the views for a data item in a view holder
+//    public static class ViewHolder extends RecyclerView.ViewHolder {
+//        // each data item is just a string in this case
+//        public TextView mTextView;
+//        public ViewHolder(TextView v) {
+//            super(v);
+//            mTextView = v;
+//        }
+//    }
+//
+//    // Provide a suitable constructor (depends on the kind of dataset)
+//    public AlarmAdapter(String[] myDataset) {
+//        mDataset = myDataset;
+//    }
+//
+//    // Create new views (invoked by the layout manager)
+//    @Override
+//    public AlarmAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+//                                                   int viewType) {
+//
+//        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+//        View view = layoutInflater.inflate(R.layout.cardview_layout, parent, false);
+//        ViewHolder viewHolder = new ViewHolder(view);
+//
+//        return viewHolder;
+//    }
+//
+//    // Replace the contents of a view (invoked by the layout manager)
+//    @Override
+//    public void onBindViewHolder(ViewHolder holder, int position) {
+//        // - get element from your dataset at this position
+//        // - replace the contents of the view with that element
+//        holder.mTextView.setText(mDataset[position]);
+//
+//    }
+//
+//    // Return the size of your dataset (invoked by the layout manager)
+//    @Override
+//    public int getItemCount() {
+//        return mDataset.length;
+//    }
+//}}
